046009876c41a29386887fc2c6a9bc403d4f436d
Matthew Honan <mhona002@fiu.edu>
2022-11-24 14:19:17 -0500

    first commit

--- src/api.js
+++ src/api.js
@@ -8,5 +8,8 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const NEWS_API_KEY = "7af55e08a0684d7c98f9bf8a8fff0588";
+export const NEWS_API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "f7bd14369a1fe422e8ccb4441d3a2a46";
--- src/components/newslist/NewsList.js
+++ src/components/newslist/NewsList.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import NewsItem from './NewsItem';
+import { NEWS_API_KEY, NEWS_API_URL } from '../../api';
 const NewsList = () => {
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
         const getArticles = async ()=> {
-            const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=7af55e08a0684d7c98f9bf8a8fff0588')
+            const response = await axios.get(`${NEWS_API_URL}${NEWS_API_KEY}`)
             console.log(response)
             setArticles(response.data.articles)
         }
